2fca8b4ef9687a8cf92dbfe16a8594725db79311
kitarooo <bravlstarstop05@mail.ru>
2024-01-27 17:08:39 +0300

    Initial commit

---
+++ src/main/java/aza/backend/halyklife/HalykLifeApplication.java
@@ -0,0 +1,13 @@
+package aza.backend.halyklife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalykLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalykLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aza/backend/halyklife/HalykLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package aza.backend.halyklife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalykLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
